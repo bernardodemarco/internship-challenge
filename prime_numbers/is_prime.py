8e656cc8aed066248b48ae4fa78aa836058c6a02
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-11-12 09:43:56 -0300

    clean primes code

--- prime_numbers/is_prime.py
+++ prime_numbers/is_prime.py
@@ -1,15 +1,5 @@
-'''
-2 - Números primos
-    -- A função deve receber um numero N > 1 (validar o input), e retornar todos os números primos até o numero N. EX. p(2) = [2]; p(3) = [2, 3]; p(10) = [2, 3, 5, 7];
-    --- Criar uma função recursiva que resolva p
-    --- Criar uma função linear que resolva p
-
-    --- REVISAR CODIGO
-    - VER PONTOS DE MELHORA
-    - ADICIONAR MENU
-    - PASSAR FUNC GENERICA COMO PARAMETRO PARA get_prime_numbers_up_to_target
-'''
 import math
+
 
 def is_prime(num: int) -> bool:
     if num < 2 or (num > 2 and num % 2 == 0):
@@ -23,6 +13,3 @@ def is_prime(num: int) -> bool:
             return False
         
     return True
-
-
-
